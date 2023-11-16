c56500d933cb551dbd2e0f469035c7e51f3664b0
Abdurrahman SASTIM <abdurrahman.sastim@ledger.fr>
2023-11-16 21:26:25 +0100

    test: add B2CQA-1752 and B2CQA-364 tests

--- apps/ledger-live-mobile/e2e/models/wallet/portfolioPage.ts
+++ apps/ledger-live-mobile/e2e/models/wallet/portfolioPage.ts
@@ -23,6 +23,7 @@ export default class PortfolioPage {
   sendTransferMenuButton = () => getElementById("transfer-send-button");
   sendMenuButton = () => getElementById("send-button");
   marketTabButton = () => getElementById("tab-bar-market");
+  walletTabMarket = () => getElementById("wallet-tab-Market");
   earnButton = () => getElementById("tab-bar-earn");
 
   navigateToSettings() {
@@ -74,6 +75,10 @@ export default class PortfolioPage {
 
   openMarketPage() {
     return tapByElement(this.marketTabButton());
+  }
+
+  openWalletTabMarket() {
+    return tapByElement(this.walletTabMarket());
   }
 
   openMyLedger() {
